c2209b811197e1b875810b56bfa074a99993feaa
22SOECE11080 <135407619+22SOECE11080@users.noreply.github.com>
2024-12-07 16:06:52 +0530

    code

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:gadhiyaaxit@gmail.com.com">gadhiyaaxit@gmail.com.com</a>
+         <a href="mailto:uttamsojitra1204@gmail.com">uttamsojitra1204@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Rajkot, india - 360003</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="call:9016425677">9016425677</a>
+         <a href="call:9510488194">9510488194</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr.Axit Gadhiya & Mr.Uttam Sojitra</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
